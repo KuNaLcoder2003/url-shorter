cd7b574ca732582a304fd86a1711df67b44d08d6
Kunal Singh <kunalindia59@gmail.com>
2026-06-16 22:28:42 +0530

    Removed get route

--- backend/src/routes/url.route.ts
+++ backend/src/routes/url.route.ts
@@ -1,10 +1,10 @@
 import express from "express"
-import { createShortUrl, getUrl } from "../controllers/url.controller.js";
+import { createShortUrl } from "../controllers/url.controller.js";
 import { rateLimitter } from "../middlewares/rateLimitter.js";
+import { authMiddleWare } from "../middlewares/authMiddleware.js";
 
 const urlRouter = express.Router()
 
-urlRouter.post('/new-url', createShortUrl)
-urlRouter.get('/:nanoId', rateLimitter, getUrl)
+urlRouter.post('/new-url', authMiddleWare, createShortUrl)
 
 export default urlRouter;
