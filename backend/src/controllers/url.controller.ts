65bea0836911c1362aecb62c1b6bdd0ab0c058a2
Kunal Singh <kunalindia59@gmail.com>
2026-06-16 22:28:18 +0530

    Removed get route and added that in index.ts file

--- backend/src/controllers/url.controller.ts
+++ backend/src/controllers/url.controller.ts
@@ -4,10 +4,10 @@ import { prisma } from "../db.js"
 import { url_read_cache } from "../config/redis.js"
 import { getAnalytics } from "../utility/analytics.js"
 import { analytics_queue } from "../config/bullMq.js"
-export const createShortUrl = async (req: express.Request, res: express.Response) => {
+export const createShortUrl = async (req: any, res: express.Response) => {
     try {
-        const email = "kunalindia59@gmail.com"
-        const user_id = "f6bc5f18-bbbe-46e9-96c3-002caf9a83df"
+        const email = req.email
+        const user_id = req.id
         const { url } = req.body
         if (!url) {
             res.status(400).json({
@@ -49,55 +49,55 @@ export const createShortUrl = async (req: express.Request, res: express.Response
     }
 }
 
-export const getUrl = async (req: express.Request, res: express.Response) => {
-    try {
-        // get the nanoId
-        const nanoId = req.params.nanoId as string
-        if (!nanoId) {
-            res.status(400).send("Rquested page not available")
-            return
-        }
+// export const getUrl = async (req: express.Request, res: express.Response) => {
+//     try {
+//         // get the nanoId
+//         const nanoId = req.params.nanoId as string
+//         if (!nanoId) {
+//             res.status(400).send("Rquested page not available")
+//             return
+//         }
 
 
-        const cache_hit = await url_read_cache.get(nanoId)
-        // write analytics job to queue
-        const analytics = getAnalytics(req.ip as string, req.headers['user-agent'] as string)
-        const final_analytics = {
-            ...analytics,
-            ip_address: req.ip,
+//         const cache_hit = await url_read_cache.get(nanoId)
+//         // write analytics job to queue
+//         const analytics = getAnalytics(req.ip as string, req.headers['user-agent'] as string)
+//         const final_analytics = {
+//             ...analytics,
+//             ip_address: req.ip,
 
-        }
-        await analytics_queue.add("analytics_job", JSON.stringify({ nanoId: nanoId, analytics: final_analytics }))
-        if (cache_hit) {
-            const { redirect_url } = JSON.parse(cache_hit)
-            res.redirect(redirect_url)
-            return
-        }
+//         }
+//         await analytics_queue.add("analytics_job", JSON.stringify({ nanoId: nanoId, analytics: final_analytics }))
+//         if (cache_hit) {
+//             const { redirect_url } = JSON.parse(cache_hit)
+//             res.redirect(redirect_url)
+//             return
+//         }
 
-        // fetch from DB
-        const response = await prisma.$transaction(async (tx) => {
-            const url = await tx.nanoIds.findFirst({
-                where: {
-                    nano_id: nanoId
-                },
-                select: {
-                    redirect_url: true
-                }
-            })
-            return url
-        }, { timeout: 10000, maxWait: 5000 })
+//         // fetch from DB
+//         const response = await prisma.$transaction(async (tx) => {
+//             const url = await tx.nanoIds.findFirst({
+//                 where: {
+//                     nano_id: nanoId
+//                 },
+//                 select: {
+//                     redirect_url: true
+//                 }
+//             })
+//             return url
+//         }, { timeout: 10000, maxWait: 5000 })
 
-        if (!response?.redirect_url) {
-            res.status(404).send("Rquested page not available")
-            return
-        }
-        // write to cache 
-        await url_read_cache.set(nanoId, JSON.stringify({ redirect_url: response.redirect_url }), "EX", 43200)
-        res.redirect(response.redirect_url)
+//         if (!response?.redirect_url) {
+//             res.status(404).send("Rquested page not available")
+//             return
+//         }
+//         // write to cache 
+//         await url_read_cache.set(nanoId, JSON.stringify({ redirect_url: response.redirect_url }), "EX", 43200)
+//         res.redirect(response.redirect_url)
 
-    } catch (error) {
-        console.log(error)
-        res.status(500).send("Something went wrong")
-        return
-    }
-}
+//     } catch (error) {
+//         console.log(error)
+//         res.status(500).send("Something went wrong")
+//         return
+//     }
+// }
